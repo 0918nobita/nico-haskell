c24a6d5296a258f6b19447ca37ed5ffa90c8aef7
0918nobita <nobita.0918@gmail.com>
2018-11-09 01:58:45 +0900

    :pencil: 半群, 結合法則について追記した

--- example.hs
+++ example.hs
@@ -22,5 +22,33 @@
 -- 半群 = 足し合わせることができる構造
 -- 半群 (semigroup) は集合 S とその上の結合的二項演算とをあわせて考えた代数的構造である。
 
+import Prelude hiding (concat)
+
 class Semigroup a where
   (<>) :: a -> a -> a
+
+-- 結合法則
+associativeLaw :: (Semigroup a, Eq a) => a -> a -> a -> Bool
+associativeLaw x y z = (x <> y) <> z == x <> (y <> z)
+
+-- 二項演算 ( a -> b -> c )
+
+-- 『上の』＝『に閉じている』
+-- + は Integer 上の演算
+-- + は Integer について閉じている
+
+instance Semigroup Integer where
+  (<>) = (+)
+
+instance Semigroup Bool where
+  (<>) = (&&)
+
+-- 整数の総和を求める関数
+concat :: Semigroup a => a -> [a] -> a
+concat = foldl (<>)
+
+resultSum :: Integer
+resultSum = concat 0 [1..100]
+
+resultAll :: Bool
+resultAll = concat True [True, True, True]
